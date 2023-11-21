ed1ec0fd000f3eadc602951694f40869a4be6e7f
kdturkar <kdturkar.97@gmail.com>
2023-11-21 10:38:04 +0530

    fourth-commit

--- 17-fragments/src/App.jsx
+++ 17-fragments/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <h1>Healthy Food</h1>
       <ul class="list-group">
         <li class="list-group-item">An 1st item</li>
-        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A 2nd item</li>
         <li class="list-group-item">A third item</li>
         <li class="list-group-item">A fourth item</li>
         <li class="list-group-item">And a fifth one</li>
