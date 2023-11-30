e58399282ccd21be331c9336d9a98911839f7c68
kdturkar <kdturkar.97@gmail.com>
2023-11-30 22:50:15 +0530

    commit

--- 17-fragments/src/App.jsx
+++ 17-fragments/src/App.jsx
@@ -3,14 +3,18 @@ import React from "react";
 function App() {
   let foodItems = [];
   // let foodItems = ["Dal", "Green Veggies", "Roti", "Salad", "Milk"];
+  
   // if (foodItems.length > 0) {
   //   return <h3>Lenght is &gt 0</h3>
   // }
-  let exprsn = foodItems.length > 0 ? null : <h3>Ohh Shitt..</h3>
-  return (
+
+  // let exprsn = foodItems.length > 0 ? null : <h3>Ohh Shitt..</h3>
+  
+  
+  return (     
     <>
       <h1>Healthy Food</h1>
-      {exprsn}
+      {foodItems.length === 0 && <h3>Ohh Shitt..</h3>}
       <ul className="list-group">
         {foodItems.map((foodItem) => (
           <li key={foodItem} className="list-group-item">
