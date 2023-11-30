b06be91254668cbe5e8165ffaee434f8ec1031ac
kdturkar <kdturkar.97@gmail.com>
2023-11-30 22:38:52 +0530

    commit

--- 17-fragments/src/App.jsx
+++ 17-fragments/src/App.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 
 function App() {
+  let foodItems = [];
+  // let foodItems = ["Dal", "Green Veggies", "Roti", "Salad", "Milk"];
+  // if (foodItems.length > 0) {
+  //   return <h3>Lenght is &gt 0</h3>
+  // }
+  let exprsn = foodItems.length > 0 ? null : <h3>Ohh Shitt..</h3>
   return (
     <>
       <h1>Healthy Food</h1>
-      <ul class="list-group">
-        <li class="list-group-item">An 1st item</li>
-        <li class="list-group-item">A 2nd item</li>
-        <li class="list-group-item">A 3rd item</li>
-        <li class="list-group-item">A 4th item</li>
-        <li class="list-group-item">And a 5th one</li>
+      {exprsn}
+      <ul className="list-group">
+        {foodItems.map((foodItem) => (
+          <li key={foodItem} className="list-group-item">
+            {foodItem}
+          </li>
+        ))}
       </ul>
     </>
-  )
+  );
 }
-export default App; 
+export default App;
