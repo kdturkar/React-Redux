964456d9b947536170e14917c34af3a7b3473b12
kdturkar <kdturkar.97@gmail.com>
2023-12-17 12:06:23 +0530

    commited

--- 17-fragments/src/App.jsx
+++ 17-fragments/src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import FoodItems from "./components/FoodItems";
 import ErrorMsg from "./components/ErrorMsg";
+import Container from "./components/Container";
 
 function App() {
   // let foodItems = [];
   let foodItems = ["Dal", "Green Veggies", "Roti", "Salad", "Milk"];
-  return (
-    <>
+  return (      
+    <Container>  
       <h1>Healthy Food</h1>
       <ErrorMsg items={foodItems}></ErrorMsg>
       <FoodItems items={foodItems}></FoodItems>
-    </>
+    </Container>
   );
 }
 export default App;
