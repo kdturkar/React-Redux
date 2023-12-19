95cf8a91d356378e8229151e17c7e5e2f13e04ee
Krunal <kdturkar.97@gmail.com>
2023-12-19 22:26:36 +0530

    commited

--- 17-fragments/src/components/FoodItems.jsx
+++ 17-fragments/src/components/FoodItems.jsx
@@ -2,14 +2,21 @@ import Item from "./Item";
 
 const FoodItems = (props) => {
   let { items } = props;
-  const handleBuyBtn = () => {
-    console.log("Item bought " + items);
-  };
+
+  // It is a behaviour of 'handleBuyBtn' which is passed as a props in 'Item' component.
+  // const handleBuyBtn = () => {
+  //   console.log("Item bought " + items);
+  // };
+
   return (
     <ul className="list-group">
       {items.map((item) => (
         // foodItem is kind of argument to Item component
-        <Item key={item} foodItem={item} handleBuyBtn={handleBuyBtn}></Item>
+        <Item key={item} foodItem={item} handleBuyBtn= {
+          () => {
+            console.log(`Item bought ${item}`)
+          }
+        } ></Item>
       ))}
     </ul>
   );
--- 17-fragments/src/components/Item.jsx
+++ 17-fragments/src/components/Item.jsx
@@ -3,19 +3,18 @@ import styles from "./Item.module.css";
 // Here we are catching foodItem as a props.
 // Props is kind of object that holds all possible parameers.
 // We can give any name to props.
-const Item = ({ foodItem , handleBuyBtn}) => {
+const Item = ({ foodItem, handleBuyBtn }) => {
   // Array destructing
   // let {foodItem} = props;
 
-  const handleBtn = (event) => {
-    console.log(event);
-    handleBuyBtn(event);
-  }
+  // const handleBtn = (event) => {
+  //   console.log(event.target.value);
+  // }
 
   return (
     <li className={`${styles["li-rw"]} ${styles["list-group-item"]}`}>
       <span className={styles["li-content"]}>{foodItem}</span>
-      <button type="button" onClick={(event) => handleBtn(event)} className={`${styles["buy-btn"]} btn btn-info`}>Buy</button>
+      <button type="button" onClick={handleBuyBtn} className={`${styles["buy-btn"]} btn btn-info`}>Buy</button>
     </li>
   );
 };
