404b1692a5f83289bf19bf1ad345824618b41aeb
kdturkar <kdturkar.97@gmail.com>
2023-12-19 19:04:05 +0530

    c

--- 17-fragments/src/components/FoodItems.jsx
+++ 17-fragments/src/components/FoodItems.jsx
@@ -2,11 +2,14 @@ import Item from "./Item";
 
 const FoodItems = (props) => {
   let { items } = props;
+  const handleBuyBtn = () => {
+    console.log("Item bought " + items);
+  };
   return (
     <ul className="list-group">
       {items.map((item) => (
         // foodItem is kind of argument to Item component
-        <Item key={item} foodItem={item}></Item>
+        <Item key={item} foodItem={item} handleBuyBtn={handleBuyBtn}></Item>
       ))}
     </ul>
   );
