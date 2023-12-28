596b73bdb11994fc644a9961ce7b9520693b75dd
kdturkar <kdturkar.97@gmail.com>
2023-12-28 10:41:22 +0530

    C

--- Projects/1-todo-app-v3/src/components/AddToDo.jsx
+++ Projects/1-todo-app-v3/src/components/AddToDo.jsx
@@ -1,24 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { IoMdAddCircle } from "react-icons/io";
 
 function AddToDo({ handleFetchData }) {
-  let [todoName, setTodoName] = useState("");
-  let [todoDate, setTodoDate] = useState("");
-
-  const handleNameChange = (e) => {
-    setTodoName(e.target.value);
-  };
-
-  const handleDateChange = (e) => {
-    setTodoDate(e.target.value);
-  };
+  const toDoNameElement = useRef();
+  const todoDateElement = useRef();
 
   const handleAddBtn = (e) => {
-    console.log(e);
     e.preventDefault(); // Server ko data nahi bhejta, to ab item immediately gayab nahi hota
-    handleFetchData(todoName, todoDate);
-    setTodoName("");
-    setTodoDate("");
+    handleFetchData(toDoNameElement.current.value, todoDateElement.current.value);
+    toDoNameElement.current.value = "";
+    todoDateElement.current.value = "";
   };
 
   return (
@@ -28,22 +19,18 @@ function AddToDo({ handleFetchData }) {
           <div className="col-4">
             <input
               type="text"
-              value={todoName}
-              onChange={(e) => handleNameChange(e)}
+              ref={toDoNameElement}
               placeholder="Enter Todo here..."
             />
           </div>
           <div className="col-4">
             <input
               type="date"
-              value={todoDate}
-              onChange={(e) => handleDateChange(e)}
+              ref={todoDateElement}
             />
           </div>
           <div className="col-4">
-            <button
-              className="btn btn-success kd-btn"
-            >
+            <button className="btn btn-success kd-btn">
               <IoMdAddCircle />
             </button>
           </div>
